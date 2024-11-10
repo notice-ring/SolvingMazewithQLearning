5d82b46f49132bd7290152634e537941e55ec7c4
Mango-Juice <wjsalsrb5@naver.com>
2024-11-11 00:47:39 +0900

    refactor codes

---
+++ gui/main_window.py
@@ -0,0 +1,89 @@
+from typing import Tuple, Dict
+from collections.abc import Callable
+import ttkbootstrap as ttk
+from wall_builder import WallBuilder
+
+from pages import InitPage, WallPage, StartPage, GoalPage, EndPage
+
+WINDOW_SIZE = 700
+MINROW = MINCOL = 3
+MAXROW = MAXCOL = 11
+
+
+class MainWindow:
+    def __init__(
+        self,
+        title: str = "Maze Solver",  # 창 제목
+        size: int = 700,  # 창 크기
+        row_cnt: Tuple[int] = (MINROW, MAXROW),  # 미로 열 개수 상/하한
+        col_cnt: Tuple[int] = (MINCOL, MAXCOL),  # 미로 행 개수 상/하한
+        on_finish: Callable[[Dict], None] = None,  # 완료시 호출할 함수
+    ) -> "MainWindow":
+        self.__size = size
+        self.__row_cnt = row_cnt
+        self.__col_cnt = col_cnt
+        self.__on_finish = on_finish
+        self.__result = {}
+
+        # 윈도우 생성
+        self.__root = ttk.Window(themename="journal")
+        self.__root.title(title)
+        self.__root.geometry(f"{size}x{size}")
+
+        # 기본 틀 생성
+        self.__main_label = ttk.Label(self.__root, text="")
+        self.__main_label.pack(pady=10)
+
+        self.__main_button = ttk.Button(
+            self.__root, text="Next", bootstyle="success", command=self.__on_next
+        )
+        self.__main_button.pack()
+
+        # 페이지 설정
+        self.__pages = [
+            InitPage(self.__root, self.__main_label, {"row": row_cnt, "col": col_cnt}),
+            WallPage(self.__root, self.__main_label),
+            StartPage(self.__root, self.__main_label),
+            GoalPage(self.__root, self.__main_label),
+            EndPage(self.__root, self.__main_label),
+        ]
+        self.__now_page = 0
+        self.__wall_builder = None
+
+        # 첫 페이지 실행
+        self.__pages[self.__now_page].show()
+
+    # 메인 윈도우를 보여주는 함수
+    def show(self):
+        self.__root.mainloop()
+
+    def __on_next(self):
+        page = self.__pages[self.__now_page]
+
+        if not page.dismiss():
+            return
+
+        # TODO: Wall Builder 부분이 뭔가 이곳 저곳에 있어서 뭔가 이상함
+        data = page.extract_data()
+        if self.__now_page == 0:
+            self.__wall_builder = WallBuilder(
+                self.__root,
+                int(data["row"]),
+                int(data["col"]),
+                width=(self.__size - 200),
+                height=(self.__size - 200),
+            )
+        else:
+            self.__result.update(data)
+
+        if self.__now_page == len(self.__pages) - 1:
+            self.__on_finish(self.__result)
+            self.__root.destroy()
+            return
+        self.__now_page += 1
+        self.__pages[self.__now_page].show(self.__wall_builder)
+
+
+if __name__ == "__main__":
+    mw = MainWindow(on_finish=lambda x: print(x))
+    mw.show()
---
+++ gui/pages.py
@@ -0,0 +1,160 @@
+from typing import Dict
+import ttkbootstrap as ttk
+from wall_builder import WallBuilder
+from design_option import DesignOption
+import tkinter.messagebox as msgbox
+
+
+class Page:
+    def __init__(
+        self,
+        root: ttk.Window,
+        label: ttk.Label,
+        description: str,
+        design_option: DesignOption = DesignOption(),
+    ) -> "Page":
+        self.__root = root
+        self.__label = label
+        self.__description = description
+        self.__design_option = design_option
+
+    @property
+    def root(self) -> ttk.Window:
+        return self.__root
+
+    @property
+    def design_option(self) -> DesignOption:
+        return self.__design_option
+
+    def show(self) -> None:
+        self.__label.config(text=self.__description)
+
+    def dismiss(self) -> bool:
+        return True
+
+    def extract_data(self) -> Dict:
+        raise NotImplementedError
+
+
+class InitPage(Page):
+    def __init__(self, root: ttk.Window, label: ttk.Label, options: Dict) -> "InitPage":
+        super().__init__(root, label, "Select the size of the maze.")
+        self.options = options
+
+    def show(self) -> None:
+        super().show()
+        self.__frame = ttk.Frame(self.root)
+        self.__frame.pack(pady=10)
+
+        self.__row = ttk.Combobox(
+            self.__frame,
+            state="readonly",
+            values=[i for i in range(*self.options["row"])],
+        )
+        label = ttk.Label(self.__frame, text="X")
+        self.__col = ttk.Combobox(
+            self.__frame,
+            state="readonly",
+            values=[i for i in range(*self.options["col"])],
+        )
+
+        self.__row.current(0)
+        self.__col.current(0)
+        self.__row.pack(padx=10, side=ttk.LEFT)
+        label.pack(padx=10, side=ttk.LEFT)
+        self.__col.pack(padx=10, side=ttk.LEFT)
+
+    def dismiss(self) -> bool:
+        self.__frame.pack_forget()
+        return True
+
+    def extract_data(self) -> Dict:
+        return {"row": int(self.__row.get()), "col": int(self.__col.get())}
+
+
+class WallPage(Page):
+    def __init__(self, root: ttk.Window, label: ttk.Label) -> "WallPage":
+        super().__init__(root, label, "Click on the edge of the maze to build a wall.")
+
+    def show(self, wall_builder: WallBuilder) -> None:
+        super().show()
+        self.__wall_builder = wall_builder
+
+    def dismiss(self) -> bool:
+        self.__wall_builder.change_click_event()
+        return True
+
+    def extract_data(self) -> Dict:
+        return {"directions": self.__wall_builder.get_movement_data()}
+
+
+class StartPage(Page):
+    def __init__(self, root: ttk.Window, label: ttk.Label) -> "StartPage":
+        super().__init__(root, label, "Select the start point.")
+
+    def show(self, wall_builder: WallBuilder) -> None:
+        super().show()
+        self.__wall_builder = wall_builder
+
+    def dismiss(self) -> bool:
+        if self.__wall_builder.selected_area is None:
+            msgbox.showerror("Error", "Select the start point.")
+            return False
+        self.__wall_builder.add_marking(
+            *self.__wall_builder.selected_area,
+            color=self.design_option.primary_color,
+        )
+        return True
+
+    def extract_data(self) -> Dict:
+        data = {"start": self.__wall_builder.selected_area}
+        self.__wall_builder.reset_temp_marking()
+        return data
+
+
+class GoalPage(Page):
+    def __init__(self, root: ttk.Window, label: ttk.Label) -> "GoalPage":
+        super().__init__(root, label, "Select the goal point.")
+
+    def show(self, wall_builder: WallBuilder) -> None:
+        super().show()
+        self.__wall_builder = wall_builder
+
+    def dismiss(self) -> bool:
+        if self.__wall_builder.selected_area is None:
+            msgbox.showerror("Error", "Select the goal point.")
+            return False
+        self.__wall_builder.add_marking(
+            *self.__wall_builder.selected_area,
+            color=self.design_option.secondary_color,
+        )
+        return True
+
+    def extract_data(self) -> Dict:
+        data = {"goal": self.__wall_builder.selected_area}
+        self.__wall_builder.reset_temp_marking()
+        return data
+
+
+class EndPage(Page):
+    def __init__(self, root: ttk.Window, label: ttk.Label) -> "EndPage":
+        super().__init__(root, label, "Select the death point.")
+
+    def show(self, wall_builder: WallBuilder) -> None:
+        super().show()
+        self.__wall_builder = wall_builder
+
+    def dismiss(self) -> bool:
+        if self.__wall_builder.selected_area is None:
+            msgbox.showerror("Error", "Select the death point.")
+            return False
+        self.__wall_builder.add_marking(
+            *self.__wall_builder.selected_area,
+            color=self.design_option.tertiary_color,
+        )
+        return True
+
+    def extract_data(self) -> Dict:
+        data = {"end": self.__wall_builder.selected_area}
+        self.__wall_builder.reset_temp_marking()
+        return data
